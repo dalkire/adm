97a575178769eeb26bb7bf9eb38e4df0924912bd
David Alkire <dalkire@npr.org>
2015-07-07 07:51:06 -0400

    stack.c

---
+++ stack.c
@@ -0,0 +1,24 @@
+/* Linked list implementation of a stack.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stack {
+	int value;
+	struct stack next;
+} stack;
+
+stack *pop(stack *s);
+
+int main(int argc, char *argv[])
+{
+	
+
+	return 0;
+}
+
+stack *pop(stack **s)
+{
+	
+}
