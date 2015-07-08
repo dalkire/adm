890636309ebb0e9fd4c12f7ea515f5b24f1cb96c
David Alkire <dalkire@npr.org>
2015-07-07 20:29:14 -0400

    stack

--- stack.c
+++ stack.c
@@ -5,20 +5,44 @@
 #include <stdlib.h>
 
 typedef struct stack {
-	int value;
-	struct stack next;
+	int item;
+	struct stack *next;
 } stack;
 
-stack *pop(stack *s);
+void push(stack **s, int x);
+void print_stack(stack *s);
 
 int main(int argc, char *argv[])
 {
-	
+	stack *s = NULL;
 
-	return 0;
+	push(&s, 1);
+	push(&s, 3);
+	push(&s, 5);
+	push(&s, 7);
+	push(&s, 9);
+
+	print_stack(s);
+
+	return EXIT_SUCCESS;
 }
 
-stack *pop(stack **s)
+void push(stack **s, int x)
 {
-	
+	stack *node;
+
+	node = malloc(sizeof(stack));
+	node->item = x;
+	node->next = *s;
+	*s = node;
+}
+
+void print_stack(stack *s)
+{
+	if (s->next == NULL) {
+		printf("%d\n", s->item);
+	} else {
+		printf("%d, ", s->item);
+		print_stack(s->next);
+	}
 }
